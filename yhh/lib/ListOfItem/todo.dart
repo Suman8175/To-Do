8670fce21d80932437b5b5369938d2f021ab3a15
sumandevkota@gmail.com <sumandevkota75@gmail.com>
2023-10-08 17:16:28 +0545

    Added add new cart button left form validation and delete button

---
+++ yhh/lib/ListOfItem/todo.dart
@@ -0,0 +1,7 @@
+List<Map<String, dynamic>> toDoList = [
+  {
+    'title': 'Do homework',
+    'desc': 'I have to do homework of Science,Social and Nepali asd sad aw'
+  },
+  {'title': 'Do task2', 'desc': 'I have to do 2'},
+];
--- yhh/lib/to_do_list.dart
+++ yhh/lib/to_do_list.dart
@@ -1,7 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:yhh/ListOfItem/todo.dart';
 
-class ToDoList extends StatelessWidget {
+class ToDoList extends StatefulWidget {
   const ToDoList({super.key});
+
+  @override
+  State<ToDoList> createState() => _ToDoListState();
+}
+
+class _ToDoListState extends State<ToDoList> {
+  var titlec = TextEditingController();
+  var descc = TextEditingController();
+
+  void fillTask() async {
+    return showDialog(
+      // barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          title: const Text('Add New Task'),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+          content: SizedBox(
+            height: 350,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                TextField(
+                  controller: titlec,
+                  style: const TextStyle(fontSize: 20),
+                  maxLines: 1,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      alignLabelWithHint: true,
+                      label: Text(
+                        'Task Title',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      hintText: 'Enter your task title',
+                      hintStyle: TextStyle(fontSize: 16)),
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: descc,
+                    style: const TextStyle(fontSize: 16),
+                    maxLines: 4,
+                    textInputAction: TextInputAction.done,
+                    decoration: const InputDecoration(
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      alignLabelWithHint: true,
+                      label: Text(
+                        'Task Detail',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      hintText: 'Enter your task in detail here',
+                      hintStyle: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(
+                      () {
+                        var title = titlec.text;
+                        var desc = descc.text;
+                        toDoList.add({'title': title, 'desc': desc});
+                        titlec.text = '';
+                        descc.text = '';
+                        for (var index in toDoList) {
+                          debugPrint(index['title']);
+                          debugPrint(index['desc']);
+                        }
+                        Navigator.of(context).pop();
+                      },
+                    );
+                  },
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+                  child: const Text('Add'),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +100,76 @@ class ToDoList extends StatelessWidget {
             Text('To Do List', style: Theme.of(context).textTheme.titleLarge),
         centerTitle: true,
       ),
-      body: const Center(child: Text('Hello User!')),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 24,
+          ),
+          Container(
+            padding: const EdgeInsets.only(
+              bottom: 7,
+            ),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.amber,
+                  width: 2.5, // Underline thickness
+                ),
+              ),
+            ),
+            child: const Text(
+              'Today\'s Task',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: toDoList.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 24),
+                    child: ListTile(
+                      horizontalTitleGap: 50.0,
+                      leading: const Icon(
+                        Icons.task,
+                        size: 32,
+                      ),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 24),
+                      title: Text(
+                        toDoList[index]['title'],
+                      ),
+                      subtitle: Text(
+                        toDoList[index]['desc'],
+                        maxLines: 3,
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {},
+                      ),
+                      iconColor: Colors.amber,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fillTask,
+        backgroundColor: Colors.amber,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
